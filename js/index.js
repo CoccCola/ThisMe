6aa9361fab38d2c8608efe23d5a09edd0275cb54
Cooooold <63093805+CoccCola@users.noreply.github.com>
2022-09-16 17:32:28 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,67 @@
+var $background = $(".background");
+var $tsm = $(".tsm");
+var $avatar = $(".avatar");
+var $b = $(".b");
+var $c = $(".c");
+var $wavesdown = $(".wavesdown");
+
+function run(){
+	$background.css({ 'animation-play-state': 'running' });
+	$tsm.css({ 'animation-play-state': 'running' });
+	$avatar.css({ 'animation-play-state': 'running' });
+	$b.css({ 'animation-play-state': 'running' });
+	$c.css({ 'animation-play-state': 'running' });
+	$wavesdown.css({ 'animation-play-state': 'running' });
+	clearTimeout(timeout_avatar);
+	timeout_avatar=setTimeout(avatarOut,2400);
+	clearTimeout(timeout_bc);
+	timeout_bc=setTimeout(bcOut,1500);
+	clearTimeout(timeout_tsm);
+	timeout_tsm=setTimeout(tsmOut,1200);
+}
+
+function avatarOut() {
+	$avatar.css({ 'animation-play-state': 'paused' });
+}
+function bcOut() {
+	if(flag==1){
+		$wavesdown.css({ 'display': 'none' });
+		flag=-1;
+	}else{
+		flag=1;
+	}
+	
+	$b.css({ 'animation-play-state': 'paused' });
+	$c.css({ 'animation-play-state': 'paused' });
+	$wavesdown.css({ 'animation-play-state': 'paused' });
+}
+function tsmOut() {
+	$tsm.css({ 'animation-play-state': 'paused' });
+}
+var progress = 0;
+var timeout_avatar;
+var timeout_bc;
+var timeout_tsm;
+var flag=1;
+window.onmousewheel = document.onmousewheel = (e) => {
+	if (e.wheelDelta < 0) {
+		if (progress <= 15) {
+			progress++;
+		}
+		if (progress == 1) {
+			run();
+		}
+
+	} else {
+		if (progress >= 1) {
+			progress--;
+		}
+		if(progress==1){	
+			$wavesdown.css({ 'display': 'block' });
+			run();
+			
+		}
+	}
+	console.log(progress);
+	
+}
---
+++ js/index2.js
@@ -0,0 +1,78 @@
+var $background = $(".background");
+var $tsm = $(".tsm");
+var $avatar = $(".avatar");
+var $b = $(".b");
+var $c = $(".c");
+var $wavesdown = $(".wavesdown");
+
+
+var progress = 0;
+var timeout_avatar;
+var timeout_bc;
+var timeout_wave;
+var flag=1;
+function displayNone(){
+	$wavesdown.css("display","none");
+}
+window.onmousewheel = document.onmousewheel = (e) => {
+	if (e.wheelDelta < 0) {
+		if (progress <= 15) {
+			progress++;
+		}
+		if (progress == 1) {
+			$background.css({'filter':"blur(5px)"});
+			$tsm.css({
+				'opacity':'0',
+				'transform': 'translate(-50%, -185%)'
+			});
+			$avatar.css({
+				'opacity': '1',
+				'transform': 'translate(-50%, -50%)',
+			});
+			$b.css({
+				"transform": "skewX(0deg) scale(1)",
+				"opacity": "0.9"
+			});
+			$c.css({
+				"transform": "skewY(0deg) scale(1)",
+				"opacity": "0.9"
+			});
+			$wavesdown.css({
+				"transform": "translateY(100%)",
+				"opacity": "0"
+			});
+
+		}
+
+	} else if(e.wheelDelta > 0){
+		if (progress >= 1) {
+			progress--;
+		}
+		if(progress==1){	
+			$background.css({'filter':"blur(0px)"});
+			$tsm.css({
+				'opacity':'1',
+				'transform': 'translate(-50%, -80%)'
+			});
+			$avatar.css({
+				'opacity': '0',
+				'transform': 'translate(-50%, 30%)',
+			});
+			$b.css({
+				"transform": "skewX(65deg) scale(3)",
+				"opacity": "0"
+			});
+			$c.css({
+				"transform": "skewY(-65deg) scale(3)",
+				"opacity": "0"
+			});
+			$wavesdown.css({
+				// "display":"block",
+				"transform": "translateY(0)",
+				"opacity": "1"
+			});
+		}
+	}
+	console.log(progress);
+	
+}
