0752997d8ba1de1dc29a5a47011682a963f77c3d
FengKui <fk350095980@163.com>
2022-09-17 10:28:17 +0800

    Complete the portfolio

--- js/index.js
+++ js/index.js
@@ -4,64 +4,153 @@ var $avatar = $(".avatar");
 var $b = $(".b");
 var $c = $(".c");
 var $wavesdown = $(".wavesdown");
+var $introduce = $(".introduce");
+var $stage1 = $(".stage1");
+var $stage2 = $(".stage2");
+var $stage2ul = $(".stage2 ul");
 
-function run(){
-	$background.css({ 'animation-play-state': 'running' });
-	$tsm.css({ 'animation-play-state': 'running' });
-	$avatar.css({ 'animation-play-state': 'running' });
-	$b.css({ 'animation-play-state': 'running' });
-	$c.css({ 'animation-play-state': 'running' });
-	$wavesdown.css({ 'animation-play-state': 'running' });
-	clearTimeout(timeout_avatar);
-	timeout_avatar=setTimeout(avatarOut,2400);
-	clearTimeout(timeout_bc);
-	timeout_bc=setTimeout(bcOut,1500);
-	clearTimeout(timeout_tsm);
-	timeout_tsm=setTimeout(tsmOut,1200);
-}
-
-function avatarOut() {
-	$avatar.css({ 'animation-play-state': 'paused' });
-}
-function bcOut() {
-	if(flag==1){
-		$wavesdown.css({ 'display': 'none' });
-		flag=-1;
-	}else{
-		flag=1;
-	}
-	
-	$b.css({ 'animation-play-state': 'paused' });
-	$c.css({ 'animation-play-state': 'paused' });
-	$wavesdown.css({ 'animation-play-state': 'paused' });
-}
-function tsmOut() {
-	$tsm.css({ 'animation-play-state': 'paused' });
-}
 var progress = 0;
-var timeout_avatar;
-var timeout_bc;
-var timeout_tsm;
-var flag=1;
+var flag = 1;
+
+function displayNone() {
+	$wavesdown.css("display", "none");
+}
 window.onmousewheel = document.onmousewheel = (e) => {
 	if (e.wheelDelta < 0) {
-		if (progress <= 15) {
+		if (progress <= 8) {
 			progress++;
 		}
 		if (progress == 1) {
-			run();
+			$background.css({ 'filter': "blur(5px)" });
+			$tsm.css({
+				'opacity': '0',
+				'transform': 'translate(-50%, -185%)'
+			});
+			$avatar.css({
+				'opacity': '1',
+				'transform': 'translate(-50%, -50%)',
+			});
+			$introduce.css({
+				'opacity': '1',
+				'top': '57%',
+			});
+			$b.css({
+				"transform": "skewX(0deg) scale(1)",
+				"opacity": "0.9"
+			});
+			$c.css({
+				"transform": "skewY(0deg) scale(1)",
+				"opacity": "0.9"
+			});
+			$wavesdown.css({
+				"transform": "translateY(100%)",
+				"opacity": "0"
+			});
 		}
+		if (progress == 7) {
+			$stage1.css({
+				"opacity": "0",
+				"z-index": "0",
+			});
 
-	} else {
+			$stage2.css({
+				"z-index": "1",
+				"opacity": "1"
+			});
+			$stage2ul.css({
+				"top": "-5vh",
+			});
+		}
+	} else if (e.wheelDelta > 0) {
 		if (progress >= 1) {
 			progress--;
 		}
-		if(progress==1){	
-			$wavesdown.css({ 'display': 'block' });
-			run();
-			
+		if (progress == 1) {
+			$background.css({ 'filter': "blur(0px)" });
+			$tsm.css({
+				'opacity': '1',
+				'transform': 'translate(-50%, -80%)'
+			});
+			$avatar.css({
+				'opacity': '0',
+				'transform': 'translate(-50%, 30%)',
+			});
+			$introduce.css({
+				'opacity': '0',
+				'top': '80%',
+			});
+			$b.css({
+				"transform": "skewX(65deg) scale(3)",
+				"opacity": "0"
+			});
+			$c.css({
+				"transform": "skewY(-65deg) scale(3)",
+				"opacity": "0"
+			});
+			$wavesdown.css({
+				// "display":"block",
+				"transform": "translateY(0)",
+				"opacity": "1"
+			});
+		}
+		if (progress == 7) {
+			$stage1.css({
+				"opacity": "1",
+				"z-index": "1",
+			});
+
+			$stage2.css({
+				"z-index": "0",
+				"opacity": "0"
+			});
+			$stage2ul.css({
+				"top": "15vh",
+			});
 		}
 	}
 	console.log(progress);
-	
+
 }
+if (sessionStorage.flag == 1) {
+	progress=8;
+	$background.css({ 'filter': "blur(5px)" });
+	$tsm.css({
+		'opacity': '0',
+		'transform': 'translate(-50%, -185%)'
+	});
+	$avatar.css({
+		'opacity': '1',
+		'transform': 'translate(-50%, -50%)',
+	});
+	$introduce.css({
+		'opacity': '1',
+		'top': '57%',
+	});
+	$b.css({
+		"transform": "skewX(0deg) scale(1)",
+		"opacity": "0.9"
+	});
+	$c.css({
+		"transform": "skewY(0deg) scale(1)",
+		"opacity": "0.9"
+	});
+	$wavesdown.css({
+		"transform": "translateY(100%)",
+		"opacity": "0"
+	});
+	$stage1.css({
+		"opacity": "0",
+		"z-index": "0",
+	});
+
+	$stage2.css({
+		"z-index": "1",
+		"opacity": "1"
+	});
+	$stage2ul.css({
+		"top": "-5vh",
+	});
+}
+$stage2.click(function() {
+	sessionStorage.flag = 1;
+})
